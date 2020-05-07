9780ae7f729eae6754d9bcdb0b2a31ad29c49280
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-07 19:13:46 +0300

    Add outputs service

---
+++ outputs/src/test/kotlin/com/example/outputs/OutputsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.outputs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OutputsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
