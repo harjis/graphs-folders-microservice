27af5f39fe4ad3a58e60eb534d403960533fd014
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-07 19:58:39 +0300

    Aand doesnt seem to work

--- kubectl-helpers/create-db.sh
+++ kubectl-helpers/create-db.sh
@@ -1,5 +1,7 @@
 kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/folders'
 kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/graphs'
+kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/outputs'
 
 kubectl rollout restart deployment/folders-deployment
 kubectl rollout restart deployment/graphs-deployment
+kubectl rollout restart deployment/outputs-deployment
