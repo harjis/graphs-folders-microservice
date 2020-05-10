206f2a007050dfee90602261458fb2fc1a2c7f67
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-10 19:20:43 +0300

    Add jobs

---
+++ jobs/src/main/kotlin/com/example/jobs/JobsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jobs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobsApplication
+
+fun main(args: Array<String>) {
+	runApplication<JobsApplication>(*args)
+}
