8908bce6c38d84cac56d5697f1ded6a2d7bb87af
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-16 11:43:12 +0300

    Add Analytics microservice

---
+++ graphs-folders-analytics/src/main/kotlin/com/example/graphsfoldersanalytics/GraphsFoldersAnalyticsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphsfoldersanalytics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphsFoldersAnalyticsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphsFoldersAnalyticsApplication>(*args)
+}
---
+++ graphs-folders-analytics/src/test/kotlin/com/example/graphsfoldersanalytics/GraphsFoldersAnalyticsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.graphsfoldersanalytics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphsFoldersAnalyticsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
