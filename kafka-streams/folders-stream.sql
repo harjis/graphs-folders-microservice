616d4c07bcf05cee90c23bfdd53302e54e2e9c22
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-19 18:13:53 +0300

    Add folder-notifications service: gradle init --dsl kotlin

--- kafka-streams/folders-stream.sql
+++ kafka-streams/folders-stream.sql
@@ -1,1 +1,12 @@
-CREATE STREAM folders WITH(kafka-topic='t_folders', value_format='AVRO');
+CREATE STREAM folders WITH (
+    kafka_topic = 't_folders',
+    value_format = 'avro'
+);
+
+CREATE TABLE folders_by_id AS
+    SELECT id,
+           latest_by_offset(name) AS name,
+           count(*) as count
+    FROM folders
+    GROUP BY id
+    EMIT CHANGES;
