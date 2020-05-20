bd4a4f95106bb39aeaccaf692d28a72a80526354
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-20 15:42:07 +0300

    Remove not needed

--- db-helpers/create-db.sh
+++ db-helpers/create-db.sh
@@ -1,7 +1,5 @@
 kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/folders'
 kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/graphs'
-kubectl exec -it deployment/postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/jobs'
 
 kubectl rollout restart deployment/folders-deployment
 kubectl rollout restart deployment/graphs-deployment
-kubectl rollout restart deployment/jobs-deployment
