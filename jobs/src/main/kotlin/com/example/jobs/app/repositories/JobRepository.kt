661369f6560d3810cc3e87afc0dbebf0499c8502
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-20 15:04:56 +0300

    Remove not needed

--- jobs/src/main/kotlin/com/example/jobs/app/repositories/JobRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jobs.app.repositories
-
-import com.example.jobs.app.entities.Job
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import java.util.*
-
-@Repository
-interface JobRepository : CrudRepository<Job, UUID>
