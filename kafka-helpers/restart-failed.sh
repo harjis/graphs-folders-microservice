8552988e1a68ae0e740101a8c1544ce94d2fb149
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-26 19:09:21 +0300

    Fis

--- kafka-helpers/restart-failed.sh
+++ kafka-helpers/restart-failed.sh
@@ -6,11 +6,13 @@ export PATH=$PATH:/usr/local/bin/
 
 date
 
-curl -s "http://localhost:8083/connectors/connectors?expand=info&expand=status" | \
+minikube_ip=$(minikube ip)
+
+curl -s "http://$minikube_ip/connectors/connectors?expand=info&expand=status" | \
            jq '. | to_entries[] | [ .value.info.type, .key, .value.status.connector.state,.value.status.tasks[].state,.value.info.config."connector.class"]|join(":|:")' | \
            column -s : -t| sed 's/\"//g'| sort
 
-curl -s "http://localhost:8083/connectors/connectors?expand=status" | \
+curl -s "http://$minikube_ip/connectors/connectors?expand=status" | \
   jq -c -M 'map({name: .status.name } +  {tasks: .status.tasks}) | .[] | {task: ((.tasks[]) + {name: .name})}  | select(.task.state=="FAILED") | {name: .task.name, task_id: .task.id|tostring} | ("/connectors/connectors/"+ .name + "/tasks/" + .task_id + "/restart")' | \
-  xargs -I{connector_and_task} curl -v -X POST "http://localhost:8083"\{connector_and_task\}
+  xargs -I{connector_and_task} curl -v -X POST "http://$minikube_ip"\{connector_and_task\}
 
