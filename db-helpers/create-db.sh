f79d20cc7cfd1e6a618d86772605c93d13e1f627
Joonas Harjukallio <harjukallio@gmail.com>
2020-05-29 16:30:35 +0300

    Easier db names

--- db-helpers/create-db.sh
+++ db-helpers/create-db.sh
@@ -1,5 +1,5 @@
-kubectl exec -it deployment/folders-postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/folders'
-kubectl exec -it deployment/graphs-postgres-deployment -- createdb -U postgres 'graphs-folders-microservice/graphs'
+kubectl exec -it deployment/folders-postgres-deployment -- createdb -U postgres 'folders-db'
+kubectl exec -it deployment/graphs-postgres-deployment -- createdb -U postgres 'graphs-db'
 
 kubectl rollout restart deployment/folders-deployment
 kubectl rollout restart deployment/graphs-deployment
