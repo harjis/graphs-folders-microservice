6f399d27d94d4d257771ccec628f71b64cb229f5
Joonas Harjukallio <harjukallio@gmail.com>
2020-08-12 16:13:45 +0300

    Use postgres

--- db-helpers/create-db.sh
+++ db-helpers/create-db.sh
@@ -1,5 +1,7 @@
 kubectl exec -it deployment/folders-postgres-deployment -- createdb -U postgres 'folders-db'
 kubectl exec -it deployment/graphs-postgres-deployment -- createdb -U postgres 'graphs-db'
+kubectl exec -it deployment/most-popular-folders-postgres-deployment -- createdb -U postgres 'most-popular-folders-db'
 
 kubectl rollout restart deployment/folders-deployment
 kubectl rollout restart deployment/graphs-deployment
+kubectl rollout restart deployment/most-popular-folders-deployment
