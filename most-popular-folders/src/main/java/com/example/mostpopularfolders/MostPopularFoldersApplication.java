bfd55a866a64c53d58277465ade40a696ad182bf
Joonas Harjukallio <harjukallio@gmail.com>
2020-08-12 10:49:37 +0300

    Add most-popular-folders service

---
+++ most-popular-folders/src/main/java/com/example/mostpopularfolders/MostPopularFoldersApplication.java
@@ -0,0 +1,13 @@
+package com.example.mostpopularfolders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostPopularFoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostPopularFoldersApplication.class, args);
+	}
+
+}
